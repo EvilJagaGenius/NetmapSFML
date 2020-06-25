8b3c04069b850ea77024fc1b022f6ed356f88edb
EvilJagaGenius <EvilJagaGenius@users.noreply.github.com>
2020-06-25 13:29:28 -0400

    Experimenting with action-definition files

--- include/programaction.h
+++ include/programaction.h
@@ -22,10 +22,14 @@ class ProgramAction {
         int range;
         int targetSprite;  // An enum might be useful for this.  0 = red (damage), 1 = green (grid), 2 = blue (buff/debuff)
         int numOfTargets;
+        vector<string> commands;  // Commands from our new action-definition files
+        vector<string> prereqs;
 
         // Functions
         ProgramAction();
+        void load(string filename);
         virtual ~ProgramAction();
+        // Should we be using these functions, or should the DB have some way of getting this info directly?
         virtual void use(Netmap_Playable* db, DataBattlePiece* source, vector<sf::Vector2i> targets);
         virtual vector<sf::Vector2i> getAimArea(sf::Vector2i origin, int targetNum);
         virtual bool checkPrereqs(DataBattlePiece* p);
--- src/programaction.cpp
+++ src/programaction.cpp
@@ -5,8 +5,57 @@ ProgramAction::ProgramAction() {
     this->numOfTargets = 1;  // 1 target by default
 }
 ProgramAction::~ProgramAction() {}
+void ProgramAction::load(string filename) {
+    // Do something, Taipu
+    cout << "Loading action\n"
+    ifstream textFile;
+    textFile.open(filename);
+    string line;
+    char loading = '0';
+
+    while (getline(textFile, line)) {
+        if (startsWith(line, "name:")) {
+            this->actionName = splitString(line, ':')[1];
+        } else if (startsWith(line, "desc:")) {
+            this->description = splitString(line, ':')[1];
+        } else if (startsWith(line, "targets:")) {
+            this->numOfTargets = stoi(splitString(line, ':')[1]);
+        } else if (startsWith(line, "range:")) {
+            this->range = stoi(splitString(line, ':')[1]);
+        } else if (startsWith(line, "sprite:")) {
+            this->targetSprite = stoi(splitString(line, ':')[1]);
+        } else if (startsWith(line, "COMMANDS")) {
+            loading = 'c';
+            continue;
+        } else if (startsWith(line, "PREREQS")) {
+            loading = 'p';
+            continue;
+        }
+        if (loading == 'c') {  // Commands
+            if (startsWith(line, "END_COMMANDS")) {
+                loading = '0';
+                continue;
+            } else {
+                this->commands.push_back(line);
+            }
+        }
+        if (loading == 'p') {  // Prereqs
+            if (startsWith(line, "END_PREREQS")) {
+                loading = '0';
+                continue;
+            } else {
+                this->prereqs.push_back(line);
+            }
+        }
+    }
+    textFile.close();
+}
+
 void ProgramAction::use(Netmap_Playable* db, DataBattlePiece* source, vector<sf::Vector2i> targets) {
     cout << "Called ProgramAction::use()\n";
+    for (string command : this->commands) {
+        cout << command << '\n';
+    }
 }
 vector<sf::Vector2i> ProgramAction::getAimArea(sf::Vector2i origin, int targetNum) {
     return getRadius(this->range, origin, false);
