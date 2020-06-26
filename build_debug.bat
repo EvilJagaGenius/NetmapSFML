e90af83540238cd59f05157b3d291f16ced7dfe5
Messmer <messmerd97@gmail.com>
2020-06-26 17:17:22 -0400

    Fixed some small issues and using some build options similar to CodeBlocks

--- build_debug.bat
+++ build_debug.bat
@@ -1,3 +1,4 @@
 mkdir bin
+mkdir bin\Debug
 mingw32-make Debug
-copy /Y .\sfml\extlibs\bin\x86\openal32.dll .\
+copy /Y .\sfml\extlibs\bin\x86\openal32.dll .\bin\Debug\ 
--- build_release.bat
+++ build_release.bat
@@ -1,3 +1,4 @@
 mkdir bin
+mkdir bin\Release
 mingw32-make
-copy /Y .\sfml\extlibs\bin\x86\openal32.dll .\
+copy /Y .\sfml\extlibs\bin\x86\openal32.dll .\bin\Release\ 
